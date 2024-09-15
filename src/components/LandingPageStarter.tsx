f75aa0575ddb93a1e2f936aac824a644580b2716
Ahmedayaz1210 <ayazm@wit.edu>
2024-09-14 23:17:37 -0400

    fixed a phone bug

--- src/components/LandingPageStarter.tsx
+++ src/components/LandingPageStarter.tsx
@@ -7,7 +7,7 @@ export default function LandingPageStarter() {
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
       <div className="container px-4 md:px-6">
-        <div className="flex flex-col md:flex-row items-center justify-end space-y-4 md:space-y-0 md:space-x-8 ml-12">
+        <div className="flex flex-col md:flex-row items-center justify-end space-y-4 md:space-y-0 md:space-x-8  lg:ml-12">
           <div className="flex flex-col items-start space-y-4 text-left md:w-1/2">
           <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none bg-gradient-to-r from-teal-600 via-blue-500 to-purple-600 text-transparent bg-clip-text">
           Elevate Your Habits with AI-Powered Guidance
